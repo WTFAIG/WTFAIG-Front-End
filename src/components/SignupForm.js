a9e6f28594875dc3e08dfdea7d3009ace336beb8
Ash Trull <ashtrull@gmail.com>
2018-01-24 16:06:17 -0500

    Add semantic LoginForm and SignupForm
    Signed-off-by: Ash Trull <ashtrull@gmail.com>

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
 const LoginForm = () => (
-  <div className='login-form'>
+  <div className='signup-form'>
     {/*
       Heads up! The styles below are necessary for the correct render of this example.
       You can do same with CSS, the main idea is that all the elements up to the `Grid`
@@ -13,11 +13,10 @@ const LoginForm = () => (
     <style>{`
       body > div,
       body > div > div,
-      body > div > div > div.login-form {
+      body > div > div > div.signup-form {
         height: 100%;
       }
-      div.login-form {
-        margin: 50px;
+      div.signup-form {
         border: none;
       }
     `}</style>
@@ -27,14 +26,38 @@ const LoginForm = () => (
       verticalAlign='middle'
     >
       <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' color='teal' textAlign='center'>
-          {' '}Log-in to your account
+        <Header as='h2' color='purple' textAlign='center'>
+          {' '}Sign up for an account
         </Header>
         <Form size='large'>
           <Segment stacked>
+          <Form.Input
+            fluid
+            icon='user'
+            iconPosition='left'
+            placeholder='Name'
+          />
+          <Form.Input
+            fluid
+            icon='home'
+            iconPosition='left'
+            placeholder='City'
+          />
+          <Form.Input
+            fluid
+            icon='home'
+            iconPosition='left'
+            placeholder='State'
+          />
+          <Form.Input
+            fluid
+            icon='home'
+            iconPosition='left'
+            placeholder='Zip'
+          />
             <Form.Input
               fluid
-              icon='user'
+              icon='mail outline'
               iconPosition='left'
               placeholder='E-mail address'
             />
@@ -45,12 +68,19 @@ const LoginForm = () => (
               placeholder='Password'
               type='password'
             />
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Confirm Password'
+              type='password'
+            />
 
-            <Button color='teal' fluid size='large'>Login</Button>
+            <Button color='purple' fluid size='large'>Signup</Button>
           </Segment>
         </Form>
         <Message>
-          New to us? <Link to="/signup">Sign Up</Link>
+          Already have an account? <Link to="/login">Login</Link>
         </Message>
       </Grid.Column>
     </Grid>
